052408c9484bf793cf22d41666d1455e4879f36b
DanyaCho <danyachoartist@gmail.com>
2025-02-20 01:38:19 +0700

    Update database.py

--- database.py
+++ database.py
@@ -128,6 +128,8 @@ def decrease_free_answers(telegram_id):
                 updated_value = cur.fetchone()
                 if updated_value:
                     logger.info(f"Бесплатные ответы уменьшены для пользователя {telegram_id}. Осталось: {updated_value[0]}")
+                else:
+                    logger.warning(f"Не удалось уменьшить free_answers_left для {telegram_id} (возможно, уже 0).")
     except psycopg2.Error as e:
         logger.error(f"Ошибка при уменьшении количества бесплатных ответов {telegram_id}: {e}")
     finally:
